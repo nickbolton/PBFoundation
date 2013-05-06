a88a2441559db37d740bc9c4da6cfd54954d12ae
nickbolton <nick@deucent.com>
2013-05-06 09:03:28 -0500

    added NSNotification+PBFoundation

---
+++ Shared/Categories/NSNotification+PBFoundation.h
@@ -0,0 +1,28 @@
+//
+//  NSNotification+Utilities.h
+//  timecop
+//
+//  Created by Nick Bolton on 6/24/12.
+//  Copyright (c) 2012 Pixelbleed LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNotification (Utilities)
+
+- (NSArray *)allManagedObjectUpdates;
+- (NSSet *)insertedManagedObjects;
+- (NSSet *)updatedManagedObjects;
+- (NSSet *)deletedManagedObjects;
+
+- (NSArray *)allManagedObjectsOfType:(Class)type;
+- (NSArray *)insertedManagedObjectsOfType:(Class)type;
+- (NSArray *)updatedManagedObjectsOfType:(Class)type;
+- (NSArray *)deletedManagedObjectsOfType:(Class)type;
+
+- (BOOL)insertedManagedObjectsContainsType:(Class)type;
+- (BOOL)updatedManagedObjectsContainsType:(Class)type;
+- (BOOL)deletedManagedObjectsContainsType:(Class)type;
+- (BOOL)allManagedObjectsContainsType:(Class)type;
+
+@end
---
+++ Shared/Categories/NSNotification+PBFoundation.m
@@ -0,0 +1,135 @@
+//
+//  NSNotification+Utilities.m
+//  timecop
+//
+//  Created by Nick Bolton on 6/24/12.
+//  Copyright (c) 2012 Pixelbleed LLC. All rights reserved.
+//
+
+#import "NSNotification+Utilities.h"
+#import "NSArray+Utilities.h"
+
+@implementation NSNotification (Utilities)
+
+- (NSSet *)insertedManagedObjects {
+    NSDictionary *userInfo = [self userInfo];
+    return [userInfo objectForKey:NSInsertedObjectsKey];
+}
+
+- (NSSet *)updatedManagedObjects {
+    NSDictionary *userInfo = [self userInfo];
+    return [userInfo objectForKey:NSUpdatedObjectsKey];
+}
+
+- (NSSet *)deletedManagedObjects {
+    NSDictionary *userInfo = [self userInfo];
+    return [userInfo objectForKey:NSDeletedObjectsKey];
+}
+
+- (NSArray *)allManagedObjectUpdates {
+    NSDictionary *userInfo = [self userInfo];
+    NSSet *inserted = [userInfo objectForKey:NSInsertedObjectsKey];
+    NSSet *updated = [userInfo objectForKey:NSUpdatedObjectsKey];
+    NSSet *deleted = [userInfo objectForKey:NSDeletedObjectsKey];
+    
+    if (inserted == nil) {
+        inserted = [NSSet set];
+    }
+    
+    if (updated == nil) {
+        updated = [NSSet set];
+    }
+    
+    if (deleted == nil) {
+        deleted = [NSSet set];
+    }
+    
+    return [NSArray arrayWithCollections:
+            inserted, updated, deleted, nil];
+}
+
+- (NSArray *)allManagedObjectsOfType:(Class)type {
+
+    NSMutableArray *result = [NSMutableArray array];
+    
+    for (id obj in [self allManagedObjectUpdates]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            [result addObject:obj];
+        }
+    }
+    return result;
+}
+
+- (NSArray *)insertedManagedObjectsOfType:(Class)type {
+
+    NSMutableArray *result = [NSMutableArray array];
+    
+    for (id obj in [self insertedManagedObjects]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            [result addObject:obj];
+        }
+    }
+    return result;
+}
+
+- (NSArray *)updatedManagedObjectsOfType:(Class)type {
+    NSMutableArray *result = [NSMutableArray array];
+    
+    for (id obj in [self updatedManagedObjects]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            [result addObject:obj];
+        }
+    }
+    return result;
+
+}
+
+- (NSArray *)deletedManagedObjectsOfType:(Class)type {
+    NSMutableArray *result = [NSMutableArray array];
+    
+    for (id obj in [self deletedManagedObjects]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            [result addObject:obj];
+        }
+    }
+    return result;
+
+}
+
+- (BOOL)insertedManagedObjectsContainsType:(Class)type {
+    for (id obj in [self insertedManagedObjects]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (BOOL)updatedManagedObjectsContainsType:(Class)type {
+    for (id obj in [self updatedManagedObjects]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (BOOL)deletedManagedObjectsContainsType:(Class)type {
+    for (id obj in [self deletedManagedObjects]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (BOOL)allManagedObjectsContainsType:(Class)type {
+    for (id obj in [self allManagedObjectUpdates]) {
+        if (type == nil || [obj isKindOfClass:type]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+@end
