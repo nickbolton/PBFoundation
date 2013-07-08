13ddbcc87d70077c482cd032ca4a47d0d2fe38ac
nickbolton <nick@deucent.com>
2013-07-07 22:07:59 -0500

    updates from prototype

---
+++ Mac/Views/PBFlippedView.h
@@ -0,0 +1,13 @@
+//
+//  PBFlippedView.h
+//  Pods
+//
+//  Created by Nick Bolton on 7/6/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PBFlippedView : NSView
+
+@end
---
+++ Mac/Views/PBFlippedView.m
@@ -0,0 +1,17 @@
+//
+//  PBFlippedView.m
+//  Pods
+//
+//  Created by Nick Bolton on 7/6/13.
+//
+//
+
+#import "PBFlippedView.h"
+
+@implementation PBFlippedView
+
+- (BOOL)isFlipped {
+    return YES;
+}
+
+@end
