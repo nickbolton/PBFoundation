a6c1e32b14ab8357023cd0c55da23c9a7089e35f
nickbolton <nick@deucent.com>
2013-12-09 20:34:18 -0600

    list view updates

---
+++ iOS/ListView/PBListViewDefaultCell.h
@@ -0,0 +1,17 @@
+//
+//  PBListViewDefaultCell.h
+//  Pods
+//
+//  Created by Nick Bolton on 12/9/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class PBListViewItem;
+
+@interface PBListViewDefaultCell : UITableViewCell
+
+@property (nonatomic, strong) PBListViewItem *item;
+
+@end
---
+++ iOS/ListView/PBListViewDefaultCell.m
@@ -0,0 +1,91 @@
+//
+//  PBListViewDefaultCell.m
+//  Pods
+//
+//  Created by Nick Bolton on 12/9/13.
+//
+//
+
+#import "PBListViewDefaultCell.h"
+#import "PBListViewItem.h"
+#import "NSLayoutConstraint+PBFoundation.h"
+
+@interface PBListViewDefaultCell()
+
+@property (nonatomic, strong) UIImageView *backgroundImageView;
+
+@end
+
+@implementation PBListViewDefaultCell
+
+- (UIImageView *)backgroundImageView {
+
+    if (_backgroundImageView == nil) {
+
+        self.backgroundImageView =
+        [[UIImageView alloc]
+         initWithImage:self.item.backgroundImage];
+
+        self.backgroundImageView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.backgroundView.alpha = 0.0;
+        self.backgroundColor = [UIColor clearColor];
+        [self.contentView addSubview:self.backgroundImageView];
+        [NSLayoutConstraint expandToSuperview:self.backgroundImageView];
+    }
+
+    return _backgroundImageView;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    [self updateBackoundImage];
+}
+
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+    [super setHighlighted:highlighted animated:animated];
+    [self updateBackoundImage];
+}
+
+- (void)updateBackoundImage {
+
+    if (self.isHighlighted) {
+
+        if (self.isSelected) {
+
+            if (self.item.highlightedSelectedBackgroundImage != nil) {
+
+                self.backgroundImageView.image =
+                self.item.highlightedSelectedBackgroundImage;
+            }
+
+        } else {
+
+            if (self.item.hightlightedBackgroundImage != nil) {
+
+                self.backgroundImageView.image =
+                self.item.hightlightedBackgroundImage;
+            }
+        }
+
+    } else {
+
+        if (self.isSelected) {
+
+            if (self.item.selectedBackgroundImage != nil) {
+
+                self.backgroundImageView.image =
+                self.item.selectedBackgroundImage;
+            }
+
+        } else {
+
+            if (self.item.backgroundImage != nil) {
+
+                self.backgroundImageView.image =
+                self.item.backgroundImage;
+            }
+        }
+    }
+}
+
+@end
